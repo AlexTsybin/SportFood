ba8606c3b19996132e8e1e1051f27a804787dd3c
alextsybin <alexander.tsybin@gmail.com>
2025-05-24 18:55:46 +0200

    Add :feature:auth module and AuthScreen

---
+++ feature/auth/src/commonMain/kotlin/com/alextsy/auth/AuthScreen.kt
@@ -0,0 +1,73 @@
+package com.alextsy.auth
+
+import ContentWithMessageBar
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.style.TextAlign
+import com.alextsy.auth.component.GoogleButton
+import com.alextsy.shared.Alpha
+import com.alextsy.shared.BebasNeueFont
+import com.alextsy.shared.FontSize
+import com.alextsy.shared.TextPrimary
+import com.alextsy.shared.TextSecondary
+import rememberMessageBarState
+
+@Composable
+fun AuthScreen(
+
+) {
+    val messageBarState = rememberMessageBarState()
+
+    Scaffold { padding ->
+        ContentWithMessageBar(
+            modifier = Modifier
+                .padding(
+                    top = padding.calculateTopPadding(),
+                    bottom = padding.calculateBottomPadding()
+                ),
+            messageBarState = messageBarState,
+            errorMaxLines = 2
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "SPORTFOOD",
+                        textAlign = TextAlign.Center,
+                        fontFamily = BebasNeueFont(),
+                        fontSize = FontSize.EXTRA_LARGE,
+                        color = TextSecondary
+                    )
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .alpha(Alpha.HALF),
+                        text = "Sign in to continue",
+                        textAlign = TextAlign.Center,
+                        fontSize = FontSize.EXTRA_REGULAR,
+                        color = TextPrimary
+                    )
+                }
+                GoogleButton(
+                    loading = false,
+                    onClick = {}
+                )
+            }
+        }
+    }
+}
